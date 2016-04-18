b7b5a10c9d71ff1fbfbaeeb812630e1517c832a0
Chih-Chun_Liu <zxcv79138246@gmail.com>
2016-04-19 01:42:53 +0800

    slider  colloge

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  var advertId = 1;
+  $('.post-arrow').click(function(){
+    if (advertId == 0) {
+      advertId = -2;
+    }else {
+      advertId++;
+    }
+    advertId = (advertId)%3;
+    var move = advertId * 960;
+    console.log('adverId:' + advertId + 'move: '+ move);
+    $('.slide').stop().animate(
+      {left: move + 'px'},
+      {
+        duration: 1000,
+        easing: 'easeInOutCubic'
+      }
+    );
+  });
+
+  $('.next-arrow').click(function(){
+    advertId--;
+    advertId = (advertId)%3;
+    var move = advertId * 960;
+    console.log('adverId:' + advertId + 'move: '+ move);
+    $('.slide').stop().animate(
+      {left: move + 'px'},
+      {
+        duration: 1000,
+        easing: 'easeInOutCubic'
+      }
+    );
+  });
+});
